3896ae61983b1fd9b43d4600c16434bc5be100be
thunder0981 <Administrator@3der-PC>
2014-04-08 16:43:04 +0800

    ff

--- src/main/java/com/test/LoginAction.java
+++ src/main/java/com/test/LoginAction.java
@@ -9,7 +9,7 @@ public class LoginAction extends ActionSupport{
 	private User user;
 	
 	public String execute() throws Exception{
-		addActionMessage( user.GetName() );
+		addActionMessage( ">>"+user.GetName() );
 		return SUCCESS;
 	}
  	public User getUser(){
